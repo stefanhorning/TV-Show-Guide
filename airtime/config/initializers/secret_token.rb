46b7d045e5d45a2e22a07b9bf2d093bcc24fb2fd
jochn <jochen.knaack@gmail.com>
2011-11-08 13:13:15 +0100

    new Rails app

--- airtime/config/initializers/secret_token.rb
+++ airtime/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Airtime::Application.config.secret_token = 'c5cb45e709eac2cc2d10650c97ef7ae04790306fbbde9d952aa636b835f88bbcadfc583c5fb7434e98f3c9ae40419c46b270ce34ed1deddd6143a3d7a5aeca9a'
+Airtime::Application.config.secret_token = '4d36d91bb073a56280cf93dac713dec07362b0c379775c79161a0301cc472e11f791325197f3d0d0cd2cf60f4fec2408f0f9e35a12c9965bad4719ebcfc06843'
