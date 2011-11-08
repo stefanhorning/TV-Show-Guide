6531db85691d1e4fe455c1eea62092079533ae0d
jochn <jochen.knaack@gmail.com>
2011-11-08 12:59:15 +0100

    App with new Rails

---
+++ airtime/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Airtime::Application.config.secret_token = 'c5cb45e709eac2cc2d10650c97ef7ae04790306fbbde9d952aa636b835f88bbcadfc583c5fb7434e98f3c9ae40419c46b270ce34ed1deddd6143a3d7a5aeca9a'
--- airtime/config/initializers/session_store.rb
+++ airtime/config/initializers/session_store.rb
@@ -1,15 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_airtime_session',
-  :secret      => 'e802329ed2c847ccac7d8a85eba8fe453927473c76234816aaa8d27abe9cdd6165d4fdf7f4c098487cf05959119e574eeebec560d3d3fed64dd37c66dfef386f'
-}
+Airtime::Application.config.session_store :cookie_store, key: '_airtime_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+# (create the session table with "rails generate session_migration")
+# Airtime::Application.config.session_store :active_record_store
