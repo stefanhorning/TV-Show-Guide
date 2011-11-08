e1efc2017032027f54a57706cee33ab392949bd3
jochn <jochen.knaack@gmail.com>
2011-11-08 13:26:52 +0100

    muuuh

--- airtime/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Airtime::Application.config.secret_token = '4d36d91bb073a56280cf93dac713dec07362b0c379775c79161a0301cc472e11f791325197f3d0d0cd2cf60f4fec2408f0f9e35a12c9965bad4719ebcfc06843'
