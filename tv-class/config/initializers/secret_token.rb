37bb9015a52c98c338a21f80fc226968f6e5f2b8
Stefan Horning <stefan@hornings.de>
2011-11-08 13:46:00 +0100

    remove tv-class

--- tv-class/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Matcher::Application.config.secret_token = '14dbcdae24aaed76cf66eed1361b53bf37d42008db0f3aa3c846c8c71703ae98b7797d2c2d840c068e67c90576c1d9fd81adb8d51973bc2224229d539a70999d'
